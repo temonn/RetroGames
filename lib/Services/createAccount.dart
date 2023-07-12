0177eaf35bd634782188921f04f6886d1193c864
temonn <t1poti00@students.oamk.fi>
2023-07-12 15:08:25 +0300

    Small fixes

--- lib/Services/createAccount.dart
+++ lib/Services/createAccount.dart
@@ -69,120 +69,111 @@ class _CreateAccountPageState extends State<CreateAccountPage> {
     _confirmPasswordController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: CustomAppBar(title: 'Create Account'),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-
-            NeonContainer(
-              spreadColor: Color(0xFF00008B),
-              borderColor: Color.fromARGB(255, 2, 2, 194),
-              containerColor: Colors.black,
-              lightBlurRadius: 20,
-              lightSpreadRadius: 10,
-              borderRadius: BorderRadius.circular(10),
-              child: TextField(
-                controller: _emailController,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(14),
-                  labelText: 'Email',
-                  labelStyle: TextStyle(
-                    color: Colors.white,
+      body: SingleChildScrollView( // Wrap with SingleChildScrollView
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              NeonContainer(
+                spreadColor: Color(0xFF00008B),
+                borderColor: Color.fromARGB(255, 2, 2, 194),
+                containerColor: Colors.black,
+                lightBlurRadius: 20,
+                lightSpreadRadius: 10,
+                borderRadius: BorderRadius.circular(10),
+                child: TextField(
+                  controller: _emailController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(14),
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-            
-            SizedBox(height: 16.0),
-
-            NeonContainer(
-              spreadColor: Color(0xFF00008B),
-              borderColor: Color.fromARGB(255, 2, 2, 194),
-              containerColor: Colors.black,
-              lightBlurRadius: 20,
-              lightSpreadRadius: 10,
-              borderRadius: BorderRadius.circular(10),
-              child: TextField(
-                controller: _usernameController,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(14),
-                  labelText: 'Username',
-                  labelStyle: TextStyle(
-                    color: Colors.white,
+              SizedBox(height: 16.0),
+              NeonContainer(
+                spreadColor: Color(0xFF00008B),
+                borderColor: Color.fromARGB(255, 2, 2, 194),
+                containerColor: Colors.black,
+                lightBlurRadius: 20,
+                lightSpreadRadius: 10,
+                borderRadius: BorderRadius.circular(10),
+                child: TextField(
+                  controller: _usernameController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(14),
+                    labelText: 'Username',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ),
-            ),
-
-            SizedBox(height: 16.0),
-
-            NeonContainer(
-              spreadColor: Color(0xFF00008B),
-              borderColor: Color.fromARGB(255, 2, 2, 194),
-              containerColor: Colors.black,
-              lightBlurRadius: 20,
-              lightSpreadRadius: 10,
-              borderRadius: BorderRadius.circular(10),
-              child: TextField(
-                controller: _passwordController,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(14),
-                  labelText: 'Password',
-                  labelStyle: TextStyle(
-                    color: Colors.white,
+              SizedBox(height: 16.0),
+              NeonContainer(
+                spreadColor: Color(0xFF00008B),
+                borderColor: Color.fromARGB(255, 2, 2, 194),
+                containerColor: Colors.black,
+                lightBlurRadius: 20,
+                lightSpreadRadius: 10,
+                borderRadius: BorderRadius.circular(10),
+                child: TextField(
+                  controller: _passwordController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(14),
+                    labelText: 'Password',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
+                  obscureText: true,
                 ),
-                obscureText: true,
               ),
-            ),
-
-            SizedBox(height: 16.0),
-
-            NeonContainer(
-              spreadColor: Color(0xFF00008B),
-              borderColor: Color.fromARGB(255, 2, 2, 194),
-              containerColor: Colors.black,
-              lightBlurRadius: 20,
-              lightSpreadRadius: 10,
-              borderRadius: BorderRadius.circular(10),
-              child: TextField(
-                controller: _confirmPasswordController,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(14),
-                  labelText: 'Confirm Password',
-                  labelStyle: TextStyle(
-                    color: Colors.white,
+              SizedBox(height: 16.0),
+              NeonContainer(
+                spreadColor: Color(0xFF00008B),
+                borderColor: Color.fromARGB(255, 2, 2, 194),
+                containerColor: Colors.black,
+                lightBlurRadius: 20,
+                lightSpreadRadius: 10,
+                borderRadius: BorderRadius.circular(10),
+                child: TextField(
+                  controller: _confirmPasswordController,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(14),
+                    labelText: 'Confirm Password',
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
+                  obscureText: true,
                 ),
-                obscureText: true,
               ),
-            ),
-
-            SizedBox(height: 16.0),
-
-            ElevatedButton(
-              onPressed: _createAccount,
-              child: Text('Create Account'),
-            ),
-
-            SizedBox(height: 16.0),
-
-            Text(
-              _errorMessage,
-              style: TextStyle(color: Colors.red),
-            ),
-          ],
+              SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: _createAccount,
+                child: Text('Create Account'),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                _errorMessage,
+                style: TextStyle(color: Colors.red),
+              ),
+            ],
+          ),
         ),
       ),
     );
